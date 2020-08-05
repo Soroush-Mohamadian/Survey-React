ee8182574ed89e487511ddc8ae270bb4dc525669
Soroush Mohamadian <soroushmdn@gmail.com>
2020-08-05 09:57:10 +0430

    data component added

---
+++ src/components/data.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+
+    let initialData={
+        organization_name:"Tap30",
+        organization_name_fa:"تپ سی",
+        logo_url:"",
+        questions:[{
+            id:"1",
+            text:"آیا از خدمات تپ سی راضی هستید؟",
+            options:[
+                {key:1,text:"زاضی ام"},
+                {key:2,text:"معمولیه"},
+                {key:3,text:"افتضاحه"}
+
+            ],
+        },
+        {
+            id:"2",
+            text:"آیا از نحوه رانندگی رانندگان تپ سی راضی هستید؟",
+            options:[
+                {key:1,text:"زاضی ام"},
+                {key:2,text:"معمولیه"},
+                {key:3,text:"افتضاحه"}
+
+            ],
+        },
+        {
+            id:"3",
+            text:"آیا از زمان بندی رانندگان تپ سی راضی هستید؟",
+            options:[
+                {key:1,text:"زاضی ام"},
+                {key:2,text:"معمولیه"},
+                {key:3,text:"افتضاحه"}
+
+            ],
+        }
+    ],
+    background_url:""
+    };
+    let outPut=[
+        {id:"",answer:1},
+        {id:"",answer:1},
+        {id:"",answer:1},
+    ]
+    return (
+        <div>
+            
+        </div>
+    )
+
+
+export default initialData
