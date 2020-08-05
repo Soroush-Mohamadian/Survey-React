f3eaef941f6fcbccf6961c0c3c56100a14e53f35
Soroush Mohamadian <soroushmdn@gmail.com>
2020-08-05 09:44:45 +0430

    initialization

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='grid-container'>
+        <div className='grid-'>
+          <img src={""} alt="profile"/>
+            <h1>Soroush MDN</h1>
+            <p>soroushmdn@gmail.com</p>
+        </div>
+        <div className='grid-'>
+          <img src={""} alt="profile"/>
+            <h1>Soroush MDN</h1>
+            <p>soroushmdn@gmail.com</p>
+        </div>
+        <div className='grid-'>
+          <img src={""} alt="profile"/>
+            <h1>Soroush MDN</h1>
+            <p>soroushmdn@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
