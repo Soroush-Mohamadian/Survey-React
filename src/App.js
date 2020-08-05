e7edba857096d42589141fa51e9619b19a3c11b9
Soroush Mohamadian <soroushmdn@gmail.com>
2020-08-05 11:02:00 +0430

    starting page created

--- src/App.js
+++ src/App.js
@@ -1,29 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+import React from 'react';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <div className='grid-container'>
-        <div className='grid-'>
-          <img src={""} alt="profile"/>
-            <h1>Soroush MDN</h1>
-            <p>soroushmdn@gmail.com</p>
-        </div>
-        <div className='grid-'>
-          <img src={""} alt="profile"/>
-            <h1>Soroush MDN</h1>
-            <p>soroushmdn@gmail.com</p>
-        </div>
-        <div className='grid-'>
-          <img src={""} alt="profile"/>
-            <h1>Soroush MDN</h1>
-            <p>soroushmdn@gmail.com</p>
-        </div>
-      </div>
+    <div id='root'>
+      <Container>
+        <Router>
+          <div>
+            <Row>
+              <Col>
+              <div className='startingPage'>
+                <h2>فرم نظرسنجی وارتقا سطح خدمات رسانی </h2>
+              </div>
+              </Col>
+            </Row>
+
+            <Row>
+            <Col>
+              <img src='./assets/Tapsi_new_logo.png'></img>
+              </Col>
+            </Row>
+
+            <Row>
+            <Col>
+            <div className="startBtnLink">
+              <Link to="/">
+                <button type="button" className="startBtn">شروع</button>
+              </Link>
+            </div>
+            </Col>
+            </Row>
+
+
+            <Row>
+            <Col>
+            <h3>قدرت گرفته از</h3>
+            </Col>
+
+            <Col>
+
+            </Col>
+            </Row>
+
+            <Row>
+            <Col>
+            <p>آنلاین CRM پلتفرم طراحی فرم های </p>
+            </Col>
+            </Row>
+
+
+
+            <Switch>
+              <Route path="/about">
+
+              </Route>
+              <Route path="/topics">
+
+              </Route>
+              <Route path="/">
+
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </Container>
     </div>
   );
 }
-
-export default App;
